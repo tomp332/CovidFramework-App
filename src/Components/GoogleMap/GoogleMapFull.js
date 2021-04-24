57baf50fd6a127dc2f2d3b371ab36cbc15599a1b
Pch12_Projects <pch12.656@gmail.com>
2021-04-24 20:31:28 +0300

    Dynamic client locations
    - Added dynamic map for new clients
    - Added popup windows for new client on map on click

--- src/Components/GoogleMap/GoogleMapFull.js
+++ src/Components/GoogleMap/GoogleMapFull.js
@@ -1,59 +1,82 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {GoogleMap, withGoogleMap, withScriptjs,Marker, InfoWindow} from "react-google-maps";
 
 
 function GlobalMap(){
-    const exampleClient = {
-        clientId:"15xz67",
-        username:"Dblet",
-        status:true,
-        lat:32.081545561398116,
-        lng:34.77447726977539
+
+    const [displayClient, setDisplayClient] = useState(false); // if to display popup for client
+    const [currentClient, setCurrentClient] = useState({}); //current client that has been clicked
+    const [clients, setClients] = useState(null); // new locations that come in from server
+
+    function getLocations()
+    {
+        fetch('http://10.0.0.4:443/api/clients/locations',{credentials:"include"})
+            .then(response=>response.json())
+            .then(clients=> {
+                setClients(clients)
+            })
     }
-    const [selectedClient, setSelectedClient] = useState(false);
-    const [currentClient, setCurrentClient] = useState(exampleClient);
+    useEffect(() => {
+        getLocations();
+        let handle = setInterval(getLocations,3000);
+        return ()=> {clearInterval(handle);
+        };
+    },[]);
+
+    function displayClientInfo()
+    {
+        let currentLat = currentClient.lat;
+        let currentLng = currentClient.lng;
+        return(
+            <>
+                {(currentLng && currentLat) &&
+                <InfoWindow
+                    onCloseClick={() => {
+                        setDisplayClient(false);
+                    }}
+                    position={{
+                        lat: currentLat,
+                        lng: currentLng
+                    }}
+                >
+                    <div>
+                        <h5>Infected client:</h5>
+                        <ul>
+                            <li>ID: {currentClient.client_id}</li>
+                            {currentClient.status ?
+                                (
+                                    <li>Status: Connected</li>
+                                ):(
+                                    <li>Status: Disconnected</li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </InfoWindow>
+                }
+            </>
+        )
+    }
+
+
     return (
         <>
             <GoogleMap defaultZoom={8} defaultCenter={{lat:31.066830,lng:34.897993}}>
+                {clients && clients.map(client => (
                 <Marker
-                    key={exampleClient.clientId}
+                    key={client.clientId}
                     position={{
-                        lat:exampleClient.lat,
-                        lng:exampleClient.lng
+                        lat:client.lat,
+                        lng:client.lng
                     }}
                     onClick={() => {
-                        setSelectedClient(true);
-                        // currentClient(exampleClient);
+                        setDisplayClient(true);
+                        setCurrentClient(client);
                     }}
 
                 />
                 ))}
-
-                {selectedClient && (
-                    <InfoWindow
-                        onCloseClick={() => {
-                            setSelectedClient(false);
-                        }}
-                        position={{
-                            lat: currentClient.lat,
-                            lng: currentClient.lng
-                        }}
-                    >
-                        <div>
-                            <h5>Infected client:</h5>
-                            <ul>
-                                <li>ID: {currentClient.clientId}</li>
-                                {currentClient.status ?
-                                    (
-                                        <li>Status: Connected</li>
-                                    ):(
-                                        <li>Status: Disconnected</li>
-                                    )
-                                }
-                            </ul>
-
-                        </div>
-                    </InfoWindow>)};
+                {displayClient && (displayClientInfo())};
             </GoogleMap>
             )}
         </>
