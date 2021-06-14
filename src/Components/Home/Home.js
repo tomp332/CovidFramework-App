92e98b927780029cbe0143b50cd81c428f200b9c
Pch12_Projects <pch12.656@gmail.com>
2021-06-14 23:31:43 +0300

    Added agent download button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import Title from '../Title/Title'
 import React, {useEffect, useState} from "react";
-import {getStatistics} from "../../api/api";
+import {downloadAgent, getStatistics} from "../../api/api";
 import {Spinner} from "reactstrap";
 import styled from '@emotion/styled';
 import {makeSelectUser} from "../../redux/selectors/userSelector";
@@ -59,6 +59,11 @@ const Home = () => {
             <ChartsWrapper>
                 {renderCharts()}
             </ChartsWrapper>
+            {/*<button className="button" onClick={(e) => downloadAgent(e)}>Download Agent</button>*/}
+            <DownloadAgentButton
+                value="Download Agent"
+                type="button" onClick={(e) => downloadAgent(e)}>Download Agent
+            </DownloadAgentButton>
         </HomePageWrapper>
 
     )
@@ -84,4 +89,13 @@ const ChartsWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+`
+
+const DownloadAgentButton = styled.button`
+  width: 10em;
+  padding: 0.5em;
+  border-radius: 5px;
+  border: none;
+  font-size: 1.25em;
+  background-color: rgba(153, 209, 75, 0.8);
 `
