12f8a3e6392a2a7565fb7ddf6befc6dbc91d395e
Pch12_Projects <pch12.656@gmail.com>
2021-06-18 18:47:43 +0300

    debugging production

--- src/Components/Clients/ClientControl/ClientControlCommand.js
+++ src/Components/Clients/ClientControl/ClientControlCommand.js
@@ -49,6 +49,7 @@ const ClientControlCommand = ({client, commands}) => {
                 const formData = new FormData();
                 formData.append("name", fileName);
                 formData.append("file", file);
+                setErrors(client.client_id)
                 let response = await uploadFile(formData, client.client_id);
                 if (!response) {
                     setErrors("Unable to upload file, please try again");
